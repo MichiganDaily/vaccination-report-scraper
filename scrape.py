4bc5e2de0b893011bf96d810023bec988c89a039
Naitian Zhou <naitian@naitian.org>
2021-07-30 16:22:24 -0700

    Update data output

--- scrape.py
+++ scrape.py
@@ -1,56 +1,74 @@
 """Scrape UM tableau for daily vaccination percentage."""
 
 from tableauscraper import TableauScraper as TS
+import json
+import requests
 import pandas as pd
 
-url = "https://tableau.dsc.umich.edu/t/UM-Public/views/U-MCovid19StudentVaccine/PUBLICThermometerbyTerm"
+url = "https://tableau.dsc.umich.edu/t/UM-Public/views/U-MCovid19StudentVaccine/PUBLICThermometer"
 
 ts = TS()
 ts.loads(url)
 
-um = ts.getWorksheet("Thermometer- PUBLIC")
-um.data.rename(
+students = ts.getWorksheet("Therm PUBLIC Students")
+students = students.data.rename(
     columns={
-        "ATTR(Max Date)-alias": "date",
-        "AGG(Vaccinated- Percent (PUBLIC))-value": "percent_vaccinated-um",
+        "Max Admin Date (today)-alias": "date",
+        "AGG(Vaccinated- Percent (PUBLIC))-value": "percent_vaccinated",
     },
-    inplace=True,
-)
+).loc[:, ["date", "percent_vaccinated"]]
+students.loc[:, "group"] = "Students"
+students.loc[:, "total_count"] = 47000  # based on 5-year avg for Fall
 
-queue = ts.getWorksheet("Thermometer- PUBLIC IN QUEUE")
-queue.data.rename(
+employees = ts.getWorksheet("Fac/Staff Therm (PUBLIC)")
+employees = employees.data.rename(
     columns={
-        "AGG(zAccessible- Vaccinated % (PUBLIC) In queue)-alias": "percent_waiting-um",
-        "ATTR(Max Date)-alias": "waiting-date",
-        "CNTD(UMID)-alias": "count_waiting-um",
+        "Fac / (Staff + Temp) (group)-value": "group",
+        "CNTD(EMPLID)-alias": "total_count",
+        "Max Admin Date (today)-alias": "date",
+        "AGG(Fully Vaccinated Percent as of Today)-value": "percent_vaccinated",
     },
-    inplace=True,
-)
+).loc[:, ["group", "total_count", "date", "percent_vaccinated"]]
 
 washtenaw = ts.getWorksheet("WashCounty Data")
-washtenaw.data.rename(
+washtenaw = washtenaw.data.rename(
     columns={
-        "Week Ending Date-alias": "washtenaw-date",
-        "SUM(MAX Sum Number of Doses)-alias": "num_doses-washtenaw",
-        "SUM(Census Mi Population 12 Years And Older)-alias": "population-washtenaw",
-        "AGG(Percent Coverage)-alias": "percent_vaccinated-washtenaw",
+        "Week Ending Date-alias": "date",
+        "SUM(Census Mi Population 12 Years And Older)-alias": "total_count",
+        "AGG(Percent Coverage)-alias": "percent_vaccinated",
     },
-    inplace=True,
-)
+).loc[:, ["date", "total_count", "percent_vaccinated"]]
+washtenaw.loc[:, "group"] = "Washtenaw"
 
-data = pd.concat(
-    [
-        um.data.loc[:, ["date", "percent_vaccinated-um"]],
-        queue.data,
-        washtenaw.data,
-    ],
-    axis=1,
-)
+all_empl = ts.getWorksheet("Employee Therm (PUBLIC)")
+all_empl = all_empl.data.rename(
+    columns={
+        "CNTD(EMPLID)-alias": "total_count",
+        '"Employee"-value': "group",
+        "Max Admin Date (today)-alias": "date",
+        "AGG(Fully Vaccinated As of Today)-alias": "count_vaxxed",
+        "AGG(Fully Vaccinated Percent as of Today)-value": "percent_vaccinated",
+    },
+).loc[:, ["group", "total_count", "date", "percent_vaccinated", "count_vaxxed"]]
 
-data.to_csv("./daily_percentage.csv", index=False)
+data = pd.concat([students, employees, washtenaw]).set_index("group")
+data.loc[:, "num_vaccinated"] = (
+    data["total_count"] * data["percent_vaccinated"]
+).astype(int)
 
+output = {}
+output["vaccination_um_data"] = data.to_dict(orient="index")
 try:
-    df = pd.read_csv("./timeseries_percentage.csv")
-    df = df.append(data)
+    risk_data = requests.get(
+        "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=integrated_county_latest_external_data"
+    ).json()
+    county = [
+        county
+        for county in risk_data["integrated_county_latest_external_data"]
+        if county["fips_code"] == 26161
+    ][0]
+    output["county_cdc_data"] = county
 except:
-    data.to_csv("./timeseries_percentage.csv", index=False)
+    print("Error getting risk data.")
+
+json.dump(output, open("daily_covid_data.json", "w"))
