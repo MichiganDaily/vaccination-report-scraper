b12ea40ec32bdc4b30bf074a026a5189fbf9a5b3
Naitian Zhou <naitian@naitian.org>
2021-09-19 20:34:23 -0400

    Fix for the dash

--- scrape.py
+++ scrape.py
@@ -11,28 +11,28 @@ url = "https://tableau.dsc.umich.edu/t/UM-Public/views/U-MCovid19StudentVaccine/
 ts = TS()
 ts.loads(url)
 
-students = ts.getWorksheet("Therm PUBLIC Students")
-students = students.data.rename(
+students = ts.getWorksheet("Therm PUBLIC Stud (ALL Status)")
+students = students.data[students.data["Verification Grouping-alias"].eq("Fully Vaccinated")].rename(
     columns={
         "Max Admin Date (today)-alias": "date",
-        "AGG(Vaccinated- Percent (PUBLIC))-alias": "percent_vaccinated",
+        "AGG(Vaccinated- Percent (PUBLIC all status))-value": "percent_vaccinated",
         # "ATTR(Vaccinated- Percent (92 FIXED PUBLIC))-alias": "percent_vaccinated"
     },
 ).loc[:, ["date", "percent_vaccinated"]]
 students.loc[:, "group"] = "Students"
 students.loc[:, "total_count"] = 50000  # based on preliminary enrollment data
 
-employees = ts.getWorksheet("Fac/Staff Therm (PUBLIC)")
-employees = employees.data.rename(
+employees = ts.getWorksheet("Fac/Staff Therm (PUBLIC all status)")
+employees = employees.data[employees.data["Verification Grouping-alias"].eq("Fully Vaccinated")].rename(
     columns={
         "Fac / (Staff + Temp) (group)-value": "group",
         "CNTD(EMPLID)-alias": "total_count",
         "Max Admin Date (today)-alias": "date",
-        "AGG(Fully Vaccinated Percent as of Today)-value": "percent_vaccinated",
+        "AGG(Vaccinated- Percent (all status))-value": "percent_vaccinated",
     },
 ).loc[:, ["group", "total_count", "date", "percent_vaccinated"]]
 
-washtenaw = ts.getWorksheet("WashCounty Data")
+washtenaw = ts.getWorksheet("WashCounty Data (2)")
 try:
     washtenaw = washtenaw.data.rename(
         columns={
@@ -45,14 +45,14 @@ try:
 except:
     print('Error getting Washtenaw data.')
 
-all_empl = ts.getWorksheet("Employee Therm (PUBLIC)")
-all_empl = all_empl.data.rename(
+all_empl = ts.getWorksheet("Employee Therm (PUBLIC all status)")
+all_empl = all_empl.data[all_empl.data["Verification Grouping-alias"].eq("Fully Vaccinated")].rename(
     columns={
         "CNTD(EMPLID)-alias": "total_count",
         '"Employee"-value': "group",
         "Max Admin Date (today)-alias": "date",
         "AGG(Fully Vaccinated As of Today)-alias": "count_vaxxed",
-        "AGG(Fully Vaccinated Percent as of Today)-value": "percent_vaccinated",
+        "AGG(Vaccinated- Percent (all status))-value": "percent_vaccinated",
     },
 ).loc[:, ["group", "total_count", "date", "percent_vaccinated", "count_vaxxed"]]
 
@@ -60,6 +60,7 @@ data = pd.concat([students, employees]).set_index("group")
 data.loc[:, "num_vaccinated"] = (
     data["total_count"] * data["percent_vaccinated"]
 ).astype(int)
+print(data)
 
 output = {}
 output["retrieved_timestamp"] = datetime.utcnow().isoformat()
